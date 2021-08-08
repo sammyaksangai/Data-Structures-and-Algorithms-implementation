89ea882f5e1a979e21c4d223e408675be7278b60
sammyaksangai <sangai.sammyak948@gmail.com>
2021-08-08 13:31:37 +0530

    Create insertionSort.py

---
+++ Sorting/insertionSort.py
@@ -0,0 +1,16 @@
+def insertionSort(arr):
+  size = len(arr)
+  for i in range(1,size):
+    #Sorted elements to the left of this key
+    key = arr[i]
+    j = i-1
+    #place the element in correct position and then place the key
+    while j>=0 and arr[j] > key:
+      arr[j+1] = arr[j]
+      j -= 1
+    arr[j+1] = key
+  print(arr)
+
+
+arr = [5,4,4,3,2,-2]
+insertionSort(arr)
